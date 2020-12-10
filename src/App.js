559d67354f072a479ba7d89319372cb36f657bde
Vilius Luneckas <vilius.luneckas@gmail.com>
2020-12-10 20:10:01 +0200

    Implement layout and product pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import pictureA from './a.jpg';
+import pictureB from './b.jpg';
+
+function cartItems() {
+	return []
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<main>
+			<header>
+				90s shop
+				<nav>
+					<ul style={{listStyleType: 'none', display: 'flex'}}>
+						<li><a href="/">Home</a></li> |
+						<li><a href="/cart">Cart ({cartItems().length})</a></li>
+					</ul>
+				</nav>
+				<hr/>
+			</header>
+
+			{
+				window.location.pathname === '/' && (
+					<div>
+						Welcome to our shop!
+
+						<p>
+							You are probably interested in <a href="/products/a">A</a>.
+						</p>
+
+						<p>
+							Check out the newest product <a href="/products/b">B</a>!
+						</p>
+					</div>
+				)
+			}
+			{
+				window.location.pathname === '/products/b' && (
+					<div>
+						<h1>Product B</h1>
+						<p>Price: 30 USD</p>
+
+						<button onClick={() => console.warn('Not implemented!')}>
+							Add to cart
+						</button>
+
+						<div><img src={pictureB} width={640}/></div>
+					</div>
+				)
+			}
+			{
+				window.location.pathname === '/products/a' && (
+					<div>
+						<h1>Product A</h1>
+						<p>Price: 10 USD</p>
+
+						<button onClick={() => console.warn('Not implemented!')}>
+							Add to cart
+						</button>
+
+						<div><img src={pictureA} width={640}/></div>
+					</div>
+				)
+			}
+			{
+				window.location.pathname === '/cart' && (
+					<div>
+						Are you ready to purchase these?
+
+						<ul>
+							{cartItems().map((cartItem) => <li key={cartItem}>{cartItem}</li>)}
+						</ul>
+					</div>
+				)
+			}
+		</main>
+	);
 }
 
 export default App;
